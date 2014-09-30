c4d30dd9c101cd00c8ca9ecd3ee64e829e189d9b
Guillermo Rauch <rauchg@gmail.com>
2014-09-30 15:08:23 -0700

    basic tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,33 @@
 
-describe('jpeg-stream', function(){
-  it('should work', function(){
+var JPEGStream = require('..');
+var assert = require('assert');
+var fs = require('fs');
+var j1 = read('1.jpg');
 
+describe('jpeg-stream', function(){
+  it('should work', function(done){
+    var parser = new JPEGStream;
+    parser.on('data', function(jpeg){
+      assert(jpeg instanceof Buffer);
+      assert(jpeg.length == j1.length);
+      done();
+    });
+    parser.write(j1.slice(0, 100));
+    process.nextTick(function(){
+      parser.write(j1.slice(100));
+    });
+  });
+
+  it('should not transform partial jpegs', function(done){
+    var parser = new JPEGStream;
+    parser.write(j1.slice(0, 100));
+    parser.on('data', function(){
+      throw new Error('Unexpected');
+    });
+    process.nextTick(done);
   });
 });
+
+function read(name){
+  return fs.readFileSync(__dirname + '/' + name);
+}
